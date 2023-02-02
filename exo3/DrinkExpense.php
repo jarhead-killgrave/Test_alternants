f3c971a9216245a7873a931a68fc8b7c8097e9a1
Manne Emile KITSOUKOU <22013393@unicaen.fr>
2023-02-02 16:51:52 +0100

    correction de l'exercice 3

--- exo3/DrinkExpense.php
+++ exo3/DrinkExpense.php
@@ -1,62 +1,22 @@
 <?php
 
-class DrinkExpense
-{
-    private float $amount;
+/**
+ * Classe représentant une dépense de boisson
+ * 
+ * Une dépense de boisson est caractérisée par :
+ * - un montant
+ * - une description
+ * - une date
+ * - un payeur
+ * - une liste de participants
+ * 
+ */
+class DrinkExpense extends Expense {
 
-    private $description;
 
-    private \DateTime $happenedAt;
 
-    private User $le_payeur;
-
-    /**
-     * @var array<User>
-     */
-    private array $participants;
-
-    /**
-     * @param array <string, User> $participants
-     */
-    public function __construct(float $amount, string $description, DateTime $happenedAt, User $le_payeur, array $participants)
+    public function getType(): string
     {
-        $this->amount = $amount;
-        $this->description = $description;
-        $this->happenedAt = $happenedAt;
-        $this->le_payeur = $le_payeur;
-        $this->participants = $participants;
-    }
-
-
-    /**
-     * @return array<string, User> $participants
-     */
-    public function getParticipants(): array
-    {
-        return $this->participants;
-    }
-
-    public function getAmount(): float
-    {
-        return $this->amount;
-    }
-
-    public function getDescription(): string
-    {
-        return $this->description;
-    }
-
-    public function getHappenedAt(): \DateTime
-    {
-        return $this->happenedAt;
-    }
-
-    public function getPayer(): User
-    {
-        return $this->le_payeur;
-    }
-
-    function get_type() {
-        return 'DRINK';
+        return "DRINK";
     }
 }
--- exo3/User.php
+++ exo3/User.php
@@ -1,34 +1,83 @@
 <?php
 
+/**
+ * Classe représentant un utilisateur
+ * 
+ * Un utilisateur est caractérisé par :
+ * - un prénom
+ * - un nom
+ * - une adresse mail
+ * 
+ */
 class User
 {
-    private int $id;
-    private $firstName;
-    private string $lastname;
 
-    public function __construct(string $firstname, string $lastname, string $mailAddress)
+    /**
+     * @var string Prénom de l'utilisateur
+     */
+    private string $firstName;
+
+    /**
+     * @var string Nom de l'utilisateur
+     */
+    private string $lastName;
+
+    /**
+     * @var string Adresse mail de l'utilisateur
+     */
+    private string $mailAddress;
+
+    /**
+     * Constructeur de la classe User
+     * 
+     * @param string $firstName Prénom de l'utilisateur
+     * @param string $lastName Nom de l'utilisateur
+     * @param string $mailAddress Adresse mail de l'utilisateur
+     */
+    public function __construct(string $firstName, string $lastName, string $mailAddress)
     {
-        $this->firstname    = $firstname;
-        $this->lastname     = $lastname;
-        $this->mailAddress= $mailAddress;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->mailAddress = $mailAddress;
     }
 
-    function getId(): int
+    /**
+     * Retourne le prénom de l'utilisateur
+     * 
+     * @return string Prénom de l'utilisateur
+     */
+    public function getFirstName(): string
     {
-        return $this->id;
+        return $this->firstName;
     }
 
-    function getFirstname(): int
+    /**
+     * Retourne le nom de l'utilisateur
+     * 
+     * @return string Nom de l'utilisateur
+     */
+    public function getLastName(): string
     {
-        return $this->firstname;
+        return $this->lastName;
     }
 
-    function getLastname(){
-        return $this->lastname;
+    /**
+     * Retourne l'adresse mail de l'utilisateur
+     * 
+     * @return string Adresse mail de l'utilisateur
+     */
+    public function getMailAddress(): string
+    {
+        return $this->mailAddress;
     }
 
-    function getMailAddress()
+    /**
+     * Retourne le nom complet de l'utilisateur
+     * 
+     * @return string Nom complet de l'utilisateur
+     */
+    public function getFullName(): string
     {
-        return $mailAddress;
+        return $this->firstName . ' ' . $this->lastName;
     }
 }
